4d4ecb065d7a370be12c24c80a1a17342a79cb80
dongbeom <rlavkgk45@gmail.com>
2026-04-20 01:11:32 +0900

    Add Hello

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,4 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    public String getGreeting() {
+        return "Hello, Git Practice!";
+    }
 }
